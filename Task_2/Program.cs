bcf95c46f138d307dbd31345c0af6bda5307e9b0
Євгеній Грищук <hryshchuk.i@donnu.edu.ua>
2020-09-19 16:05:25 +0300

    Task_2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -6,7 +6,16 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            Console.WriteLine("Введіть змінну типу int ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введіть змінну типу double ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введіть змінну типу long ");
+            long c = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine($"a={a}; b={b} ; c={c}");
+            
+
+        }   
     }
 }
